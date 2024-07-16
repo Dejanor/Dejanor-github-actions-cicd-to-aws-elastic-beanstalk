32c95324bb612dd5fa64558102768b61b3e9ff14
dejanor <emmanuelaladejana05@gmail.com>
2024-07-16 07:56:35 +0100

    modify elastic beanstalk file

--- terraform/elasticbeanstalk.tf
+++ terraform/elasticbeanstalk.tf
@@ -14,7 +14,7 @@ resource "aws_elastic_beanstalk_application" "application" {
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = var.eb-environment-name
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.83 running Docker"
+  solution_stack_name = "64bit Amazon Linux 2 v3.8.3 running Docker"
 
   setting {
     namespace = "aws:ec2:vpc"
