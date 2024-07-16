16ddbb71c1a07005ad7a33803064cb38aa1dc186
dejanor <emmanuelaladejana05@gmail.com>
2024-07-16 08:48:18 +0100

    modify elastic beanstalk file and update iam role with autoscaling

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,7 +1,5 @@
-# iam/main.tf
 resource "aws_iam_role" "ec2_role" {
   name = "aws-elasticbeanstalk-ec2-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -16,7 +14,10 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-# ... (rest of the IAM role and policy attachment code)
+resource "aws_iam_role_policy_attachment" "ec2_web_tier" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role       = aws_iam_role.ec2_role.name
+}
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "aws-elasticbeanstalk-ec2-role"
@@ -25,7 +26,6 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 
 resource "aws_iam_role" "service_role" {
   name = "aws-elasticbeanstalk-service-role-2"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -40,7 +40,44 @@ resource "aws_iam_role" "service_role" {
   })
 }
 
-# ... (rest of the service role code)
+resource "aws_iam_policy" "eb_auto_scaling_policy" {
+  name        = "eb-auto-scaling-policy"
+  path        = "/"
+  description = "Auto Scaling policy for Elastic Beanstalk"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:PutScalingPolicy",
+          "autoscaling:DeleteScalingPolicy",
+          "autoscaling:UpdateAutoScalingGroup"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eb_service_auto_scaling" {
+  policy_arn = aws_iam_policy.eb_auto_scaling_policy.arn
+  role       = aws_iam_role.service_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eb_service_health" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+  role       = aws_iam_role.service_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eb_service_general" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+  role       = aws_iam_role.service_role.name
+}
 
 output "instance_profile_name" {
   value = aws_iam_instance_profile.ec2_instance_profile.name
